eb76d8406c2233628c3da21a1f14490770a177c2
glstream <grayson.stream@gmail.com>
2022-09-14 19:16:15 -0700

    new background on cards

--- superflex/__init__.py
+++ superflex/__init__.py
@@ -62,6 +62,11 @@ def create_app(test_config=None):
         trade_datetime = datetime.utcfromtimestamp(int(str(timestamp)[:10]))
         return trade_datetime.strftime("%m-%d-%Y")
 
+    @app.template_filter("league_cat")
+    def league_cat(cat: int) -> str:
+        category = {0: "Redraft", 1: "Keeper", 2: "Dynasty"}
+        return category[cat]
+
     @app.route("/favicon.ico")
     def favicon():
         return send_from_directory(
