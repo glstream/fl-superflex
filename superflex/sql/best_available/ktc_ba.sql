7f976e5165fdf5fb9a24055cd83563d0fbc16c41
glstream <grayson.stream@gmail.com>
2023-03-04 00:15:36 -0800

    table, ba, and positional ranks

--- superflex/sql/best_available/ktc_ba.sql
+++ superflex/sql/best_available/ktc_ba.sql
@@ -22,5 +22,5 @@ and pl.player_id NOT IN (SELECT
 and pl.player_position IN ('QB', 'RB', 'WR', 'TE' )
 and pl.team is not null
 order by player_value desc) ba_t1
-where ba_t1.rn <= 5
+where ba_t1.rn <= 4
 order by ba_t1.player_position, ba_t1.player_value desc
